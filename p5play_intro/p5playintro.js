8f74d0bc2c33ae7bd10f13473cd23a2dd0ab5c64
LivelyStork <krishna48705@sas.edu.sg>
2026-07-06 11:01:53 +0000

    Jul 6, 2026, 11:01 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -1,3 +1,6 @@
+let ball;
+let box;
+
 function setup() {
   // Set up the canvas
   new Canvas(800, 400);
@@ -21,6 +24,11 @@ function setup() {
   ball.diameter = 40
   ball.color = 'blue';
 
+  ball.vel.x = 30;
+  ball.vel.y = 30;
+  ball.bounciness = 1;
+  ball.collider = "dynamic";
+
   box = new Sprite();
   box.x = 100;
   box.y = 100;
@@ -38,4 +46,9 @@ function draw() {
   if (ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2) {
   ball.vel.x *= -1;
   }
+  if (ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2) {
+  ball.vel.y *= -1;
+  }
+  box.x = mouseX;
+  box.y = mouseY;
 }
