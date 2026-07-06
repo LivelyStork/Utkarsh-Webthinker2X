302d22654805dde453576a505e1c3bcf7c814f16
LivelyStork <krishna48705@sas.edu.sg>
2026-07-06 10:34:12 +0000

    Jul 6, 2026, 10:34 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -35,4 +35,7 @@ function draw() {
   textSize(16);
   text("Ball: (" + int(ball.x) + ", " + int(ball.y) + ")", 10, 20);
   text ("Mouse: (" + mouseX + ", " + mouseY + ")", 10, 40);
+  if (ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2) {
+  ball.vel.x *= -1;
+  }
 }
