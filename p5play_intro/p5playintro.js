bc13ddba8556a926727ff05f30cd82387792017d
LivelyStork <krishna48705@sas.edu.sg>
2026-07-13 09:59:02 +0000

    Jul 13, 2026, 9:59 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -1,55 +1,55 @@
-let ball;
-let box;
+// let ball;
+// let box;
 
-function setup() {
-  // Set up the canvas
-  new Canvas(800, 400);
-  background(250); //background color
+// function setup() {
+//   // Set up the canvas
+//   new Canvas(800, 400);
+//   background(250); //background color
 
-  // Basic shape testing
-  // write your codes here
+//   // Basic shape testing
+//   // write your codes here
  
-  // End Basic shape testing
+//   // End Basic shape testing
 
-   // Create a bouncing ball sprite
-   // write your codes here
-  //  fill("skyblue")
-  //  stroke("pink")
-  //  strokeWeight(10)
-  //  ellipse(50,50,100,100)
-  //  rect(100,100,100,200)
-  ball = new Sprite();
-  ball.x = 100;
-  ball.y = 200
-  ball.diameter = 40
-  ball.color = 'blue';
+//    // Create a bouncing ball sprite
+//    // write your codes here
+//   //  fill("skyblue")
+//   //  stroke("pink")
+//   //  strokeWeight(10)
+//   //  ellipse(50,50,100,100)
+//   //  rect(100,100,100,200)
+//   ball = new Sprite();
+//   ball.x = 100;
+//   ball.y = 200
+//   ball.diameter = 40
+//   ball.color = 'blue';
 
-  ball.vel.x = 30;
-  ball.vel.y = 30;
-  ball.bounciness = 1;
-  ball.collider = "dynamic";
+//   ball.vel.x = 30;
+//   ball.vel.y = 30;
+//   ball.bounciness = 1;
+//   ball.collider = "dynamic";
 
-  box = new Sprite();
-  box.x = 100;
-  box.y = 100;
-  box.w = 50;
-  box.h = 50;
-  box.color = "green";
-}
+//   box = new Sprite();
+//   box.x = 100;
+//   box.y = 100;
+//   box.w = 50;
+//   box.h = 50;
+//   box.color = "green";
+// }
 
-function draw() {
-  // write your codes here
-  background(250)
-  fill(0);
-  textSize(16);
-  text("Ball: (" + int(ball.x) + ", " + int(ball.y) + ")", 10, 20);
-  text ("Mouse: (" + mouseX + ", " + mouseY + ")", 10, 40);
-  if (ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2) {
-  ball.vel.x *= -1;
-  }
-  if (ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2) {
-  ball.vel.y *= -1;
-  }
-  box.x = mouseX;
-  box.y = mouseY;
-}
+// function draw() {
+//   // write your codes here
+//   background(250)
+//   fill(0);
+//   textSize(16);
+//   text("Ball: (" + int(ball.x) + ", " + int(ball.y) + ")", 10, 20);
+//   text ("Mouse: (" + mouseX + ", " + mouseY + ")", 10, 40);
+//   if (ball.x < 0 + ball.diameter / 2 || ball.x > width - ball.diameter / 2) {
+//   ball.vel.x *= -1;
+//   }
+//   if (ball.y < 0 + ball.diameter / 2 || ball.y > height - ball.diameter / 2) {
+//   ball.vel.y *= -1;
+//   }
+//   box.x = mouseX;
+//   box.y = mouseY;
+// }
