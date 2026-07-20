78a2f0ac35fb064a65413dc1adba614999f53ce9
LivelyStork <krishna48705@sas.edu.sg>
2026-07-20 10:58:04 +0000

    Jul 20, 2026, 10:58 AM

--- flappybird/playground/flap1.js
+++ flappybird/playground/flap1.js
@@ -43,6 +43,6 @@ function draw() {
     fill("blue");
     textSize (14);
     text('vel.y: ' + bird.vel.y.toFixed(2), 10, 20);
-    text('isMoving: " + bird. isMoving, 10, 40') 
+    text('isMoving: " + bird. isMoving, 10, 40');
     text('sleeping: ' + bird.sleeping , 10, 60);
 }
