be731cbfe00b16a7401147dca78cb5dcd9602170
LivelyStork <krishna48705@sas.edu.sg>
2026-07-20 10:31:14 +0000

    Jul 20, 2026, 10:31 AM

--- flappybird/playground/flap1.js
+++ flappybird/playground/flap1.js
@@ -1,1 +1,10 @@
 // write your codes here
+let bird, floor; // Declaring variables for objects 
+let flapMidImg, bg, base; // Declaring variables for images
+
+function preload() {
+    Il bird image, background and the floor
+    flapMidImg = loadImage('assets/yellowbird-midflap.png');
+    bg = loadImage('assets/background-day-png');
+    base = loadImage('assets/base.png');
+}
