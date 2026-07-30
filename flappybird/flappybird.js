e4e2b9ab11e18b211a30fae22a3c946828e257a0
LivelyStork <krishna48705@sas.edu.sg>
2026-07-30 10:22:13 +0000

    Jul 30, 2026, 10:22 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -8,7 +8,7 @@ function preload() {
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
     bg = loadImage('assets/background-day.png');
     base = loadImage('assets/base.png');
-    flapUpImg = loadImage
+    flapUpImg = loadImage('')
 }
 
 function setup() {
