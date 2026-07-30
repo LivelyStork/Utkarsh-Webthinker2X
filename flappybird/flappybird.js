c95d737d839a8d8a18c1aa6588848aa5105f3fae
LivelyStork <krishna48705@sas.edu.sg>
2026-07-30 10:37:37 +0000

    Jul 30, 2026, 10:37 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -48,7 +48,7 @@ function draw() {
     textSize(14);
     text("bird.sleeping: " + bird.sleeping ,20,40)
     text("bird.vel.y" + round(bird.vel.y), 20, 60);
-    text("frameCount = " + frameCount, )
+    text("frameCount = " + frameCount, 20,80)
     // Floor to bounce bird
     if(kb.presses("space")){
         bird.vel.y = -4;
