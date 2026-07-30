d2654e22c158c9e8072a95f956075c45b14c0026
LivelyStork <krishna48705@sas.edu.sg>
2026-07-30 10:48:07 +0000

    Jul 30, 2026, 10:48 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -79,6 +79,6 @@ function draw() {
     function spawnPipePair() {
         let gap = 50
         let midY = height/2;
-        bottomPipe = new Sprite(400, someY, 52, )
+        bottomPipe = new Sprite(400, someY, 52, 320, 'static')
     }
 }
