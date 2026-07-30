211b16a0077cffbd252a034b61b26a1a907d76b5
LivelyStork <krishna48705@sas.edu.sg>
2026-07-30 09:49:37 +0000

    Jul 30, 2026, 9:49 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -35,7 +35,7 @@ function draw() {
     // test if bg is okay
     image(bg, 0, 0, width, height); // draw the background
     // Floor to bounce bird
-    let floor = new Sprite();
+    floor = new Sprite();
     floor.x = 200;
     floor.y = height - 20;
     floor.width = 400;
