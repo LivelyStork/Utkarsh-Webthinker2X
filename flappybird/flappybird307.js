ed6463c1b8d76dfd3bd151fa1f33c260529d6696
LivelyStork <krishna48705@sas.edu.sg>
2026-08-03 10:08:15 +0000

    Aug 3, 2026, 10:08 AM

--- flappybird/flappybird307.js
+++ flappybird/flappybird307.js
@@ -90,6 +90,9 @@ function draw() {
             pipe.remove();
         }
     }
+    if (bird.collides(pipeGroup) || bird.collides(floor)){
+        
+    }
 }
 function spawnPipePair() {
         let gap = 50
