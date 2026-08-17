1fa311945819f677b17ba88c550096c977255adb
LivelyStork <krishna48705@sas.edu.sg>
2026-08-17 10:23:15 +0000

    Aug 17, 2026, 10:23 AM

--- flappybird/flappybird307.js
+++ flappybird/flappybird307.js
@@ -139,6 +139,7 @@ function spawnPipePair() {
     topPipe.img = pipe;
     topPipe.rotation = 180
 }
+}
 
 function drawscore(x, y, score, digitWidth, digitHeight) {
     // Clear old digit sprites
@@ -162,7 +163,7 @@ function drawscore(x, y, score, digitWidth, digitHeight) {
     }
 }
 
-function moveGroup (group, target, spacing) {}
+function moveGroup (group, target, spacing) {
     // E.g. 3 digits → 2 gaps → (3 - 1) * 24 = 48px
     let totalwidth = (group.length -1) * spacing;
     // Find Left-most X Position
