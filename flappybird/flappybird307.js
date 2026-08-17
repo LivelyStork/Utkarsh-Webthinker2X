08b39a621845c801d7a7239f69b000ac93997952
LivelyStork <krishna48705@sas.edu.sg>
2026-08-17 10:07:43 +0000

    Aug 17, 2026, 10:07 AM

--- flappybird/flappybird307.js
+++ flappybird/flappybird307.js
@@ -159,4 +159,5 @@ function drawscore(x, y, score, digitWidth, digitHeight) {
         //get the digit image from the array based on placement order which corresponds to the digit
         digitSprite.img = numberImages[digit];
     }
+    camera.x = 
 }
