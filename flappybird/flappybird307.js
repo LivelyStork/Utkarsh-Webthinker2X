6a85b54d918cdd27763cbb211006c5bbb57bb00d
LivelyStork <krishna48705@sas.edu.sg>
2026-08-17 09:44:15 +0000

    Aug 17, 2026, 9:44 AM

--- flappybird/flappybird307.js
+++ flappybird/flappybird307.js
@@ -60,7 +60,7 @@ function setup() {
     startScreenLabel.img = startScreenImg;
     scoreDigits = new Group();
     scoreDigits.collider = 'none';
-    scoredigits.layer 
+    scoredigits.layer = 1000
 }
 
 function draw() {
