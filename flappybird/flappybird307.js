ddfd71eeac5b8f9a5eec69926892d513371d2892
LivelyStork <krishna48705@sas.edu.sg>
2026-08-17 09:50:39 +0000

    Aug 17, 2026, 9:50 AM

--- flappybird/flappybird307.js
+++ flappybird/flappybird307.js
@@ -127,9 +127,9 @@ function draw() {
 }
 function spawnPipePair() {
     let gap = random(35,60)
-        let midY = random(350, height - 350)
-        topPipe = new Sprite(bird.x + 400, midY - gap / 2 - 200, 52, 320, 'static');
-        bottomPipe = new Sprite(bird.x + 400, midY + gap / 2 + 200, 52, 320, 'static');
+    let midY = random(350, height - 350)
+    topPipe = new Sprite(bird.x + 400, midY - gap / 2 - 200, 52, 320, 'static');
+    bottomPipe = new Sprite(bird.x + 400, midY + gap / 2 + 200, 52, 320, 'static');
     pipeGroup.add(bottomPipe);
     // pipeGroup.layer = 0;
     pipeGroup.add(topPipe);
