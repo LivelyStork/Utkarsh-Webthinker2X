00d57dd351d27eb1d9c9df519c8481b8d4a5941c
LivelyStork <krishna48705@sas.edu.sg>
2026-08-17 09:41:20 +0000

    Aug 17, 2026, 9:41 AM

--- flappybird/flappybird307.js
+++ flappybird/flappybird307.js
@@ -25,6 +25,9 @@ function preload() {
     pipe = loadImage('assets/pipe-green.png');
     gameoverImg = loadImage(' assets/gameover.png'); // preload the image
     startScreenImg = loadImage(' assets/message.png'); // preload the image
+    for(let i=0; i<10; i++){
+        
+    }
 }
 
 function setup() {
