d642e27422f19ba9aadb405df54658d933f07e75
LivelyStork <krishna48705@sas.edu.sg>
2026-08-17 10:55:42 +0000

    Aug 17, 2026, 10:55 AM

--- flappybird/flappybird307.js
+++ flappybird/flappybird307.js
@@ -140,7 +140,7 @@ function draw() {
         if (pipe.passed == false && pipeRightEdge < birdLeftEdge){
             pipe.passed = true;
             score++;
-            point
+            pointSound.play();
         }
     }
 }
