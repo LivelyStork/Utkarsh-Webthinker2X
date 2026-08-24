1b438f2db90a93007909db1eb852fc7c900ac939
LivelyStork <krishna48705@sas.edu.sg>
2026-08-24 09:43:40 +0000

    Aug 24, 2026, 9:43 AM

---
+++ revision.js
@@ -0,0 +1,13 @@
+function setup () {
+    createCanvas (600,600)
+}
+function preload(){
+    //assets
+}
+function draw() {
+//animations 
+//physics
+    if(mouse.presses ()){
+        let cube = new Sprite(mouseX, mouseY, 50,50)
+    }
+}
