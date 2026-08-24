bd23d4a659835a142dff5344f7852fd690874470
LivelyStork <krishna48705@sas.edu.sg>
2026-08-24 10:00:05 +0000

    Aug 24, 2026, 10:00 AM

--- p5play_intro/revision.js
+++ p5play_intro/revision.js
@@ -3,7 +3,7 @@ function setup () {
     world.gravity.y = 10;
     let floor1 = new Sprite(width/2, height, 600, 40)
     floor1.collider = 'static'
-    let floor2 = new Sprite(width/2, height, 0, 0)
+    let floor2 = new Sprite(width/2, height, 40, 600)
     floor2.collider = 'static'
 }
 function preload(){
