c30e4652421ac3cf97347e2b3f8379f7f987b228
LivelyStork <krishna48705@sas.edu.sg>
2026-09-07 10:03:36 +0000

    Sep 7, 2026, 10:03 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -10,5 +10,5 @@ function setup (){
 }
 function draw(){
     clear(); // optional to clear before applying an image
-    image (dojoGB, 0, 0, width, height);
+    image (dojoBG, 0, 0, width, height);
 }
