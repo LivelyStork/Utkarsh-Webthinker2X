2e29eb7c788d588de068a085a620ada13d4aba28
LivelyStork <krishna48705@sas.edu.sg>
2026-09-07 10:22:59 +0000

    Sep 7, 2026, 10:22 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -29,3 +29,15 @@ function draw(){
         spawnFruit ();
     }
 }
+
+// spawnFruit function
+function spawnFruit () {V
+    let fruitData = random (fruitTypes); // pick one at random
+    let random = random(300, 500); // random X to spawn. Rem that canvas width is 800
+    let fruit = new fruitGroup.Sprite(randomX, height+20, 40); // spawn at bottom
+    fruit.image = fruitData.whole; // load the image for whole
+    fruit.type = fruitData; // store reference to its type i.e. peach or watermelon
+    fruit.vel.y = random(-10, -14); // shoot upward at random velocity. Adjust to try!
+    fruit. vel.x = random(-2, 2); // sideways curve. Adjust to try!
+    fruit.friction = 0; // no friction
+}
