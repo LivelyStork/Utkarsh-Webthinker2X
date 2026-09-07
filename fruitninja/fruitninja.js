c6700312c97ba40f6eb4c69e6217b751da432107
LivelyStork <krishna48705@sas.edu.sg>
2026-09-07 10:02:28 +0000

    Sep 7, 2026, 10:02 AM

---
+++ fruitninja/fruitninja.js
@@ -0,0 +1,13 @@
+let dojoBG; // image for the background
+
+function preload(){
+    dojoBG =loadImage('assets/dojobackground.png');
+}
+
+function setup (){
+    new Canvas (800, 600) ;
+    world.gravity.y = 10; // set gravity for the world
+}
+function draw(){
+    clear(); // optional to clear before applying an image. image (dojoBG, 0, 0, width, height);
+}
