b27c88c8bdd9110b18a7b6b5ccdf5b9f93dfb13b
LivelyStork <krishna48705@sas.edu.sg>
2026-09-14 10:25:01 +0000

    Sep 14, 2026, 10:25 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -80,3 +80,14 @@ function sliceFruit() {
         }
     }
 }
+
+function splitFruit (x, y, fruitData) {
+// Create left half
+let left = new fruitHalves.Sprite(x - 10, y, 40, 40);
+left.img = fruitData.half1;
+left.vel.x = -3; // veer left
+left.vel.y = random(-5, -2);
+left.rotationSpeed = -5;
+left.life = 30; // remove after 30 frames
+// Create right half
+}
