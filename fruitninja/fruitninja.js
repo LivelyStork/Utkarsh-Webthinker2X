516fa31f74ce72ed0cf131f2418f28a126fae677
LivelyStork <krishna48705@sas.edu.sg>
2026-09-21 10:30:43 +0000

    Sep 21, 2026, 10:30 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -86,6 +86,9 @@ function draw(){
     textAlign (LEFT, TOP);
     text("Score: " + score, 10, 10);
     text("Missed: " + missedFruits, 200, 10);
+    |\/ Timer logic: update game timer, end after 60s
+gameTimer = floor((millis() - gameStartTime) / 1000);
+text('Time: ' + (gameDuration - gameTimer), 400, 10);
 }
 
 // spawnFruit function
