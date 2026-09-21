ce2fa2a4a560296311508744ed3d108cb5e1788e
LivelyStork <krishna48705@sas.edu.sg>
2026-09-21 10:09:35 +0000

    Sep 21, 2026, 10:09 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -44,7 +44,8 @@ function draw(){
         textSize (48);
         text('Fruit Ninja', width / 2, height / 2 - 40);
         textSize (24);
-        text ('Press SPACE or Click to Start', width / 2, height / 2 + 20); return; // do not process rest of this function
+        text ('Press SPACE or Click to Start', width / 2, height / 2 + 20);
+        return; // do not process rest of this function
 }
     // call spawnFruit function
     if (frameCount % 120 === 0){
