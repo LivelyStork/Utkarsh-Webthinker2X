115ded932efba0135e9a761f19cdf199e8cacd2a
LivelyStork <krishna48705@sas.edu.sg>
2026-09-21 10:02:05 +0000

    Sep 21, 2026, 10:02 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -3,7 +3,7 @@ let fruitTypes = []; // store fruit image objects
 let fruitGroup; // Group for whole fruits
 let fruitHalves; // Group for fruit halves
 let score = 0
-let missedFruit
+let missedFruit = 0
 
 function preload(){
     dojoBG =loadImage('assets/dojobackground.png');
