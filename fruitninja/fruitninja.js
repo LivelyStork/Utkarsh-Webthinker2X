0c7dea7747a1532aa24b15eac0b41ea0a42a1078
LivelyStork <krishna48705@sas.edu.sg>
2026-09-21 09:53:26 +0000

    Sep 21, 2026, 9:53 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -46,6 +46,7 @@ function draw(){
     }
     // Display: Score, Missed, Timer
     stroke (158, 69, 69); // rgb colour fill (255); textSize (24);
+    
     textAlign (LEFT, TOP);
     text ('Score: ' + score, 10, 10);
 }
